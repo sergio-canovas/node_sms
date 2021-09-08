8ee17b16733e54090f13c6466e6307c786efdb35
sergio-canovas <sherokun@gmail.com>
2021-09-08 19:20:24 +0200

    the last commit_i hope_

--- sms.js
+++ sms.js
@@ -1,5 +1,5 @@
-const http = require("http");
-http.createServer((req,res)=>{
-    res.write("Hello World!");
-    res.end;
-}).listen(8080)
+const sms =["Are you learning node? PHP is better", "You should try Python", "Node is dead, the future is .NET","Notepad is the best IDE","The best algorythms appears in dreams"]
+
+let number= Math.floor(Math.random()*sms.length);
+
+console.log(sms[number]);
